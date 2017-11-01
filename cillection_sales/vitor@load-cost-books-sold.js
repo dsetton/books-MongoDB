ca8e9c9b0d036208d1b157988b9e42f1c246b672
Vitor <vitorcandrade98@gmail.com>
2017-11-01 18:19:59 -0300

    2- valor do custo

---
+++ cillection_sales/vitor@load-cost-books-sold.js
@@ -0,0 +1,33 @@
+var data = {
+    "_id" : 1509556487846.0,
+    "isbn" : "681-990-636-415"
+}
+
+db.runCommand({
+    aggregate: "sales",
+    pipeline: [
+        {
+            $match: {
+                "items.isbn": {
+                    $regex : ".*"+data.isbn+".*", $options : "i"
+                }
+            }
+        },
+        {
+            $project: {
+                items: {
+                    $filter: {
+                        input: "$items",
+                        as: "item",
+                        cond: { $eq: [ "$$item.isbn", data.isbn ] }
+                    }
+                }
+            }
+        },
+        {
+            $group: {
+                _id : {$sum :"$items.price"}
+            }
+        }
+    ]
+})

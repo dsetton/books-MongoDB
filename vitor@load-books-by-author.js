5b26d65104547061aac453d4db679bff6d6e612e
Vitor <vitorcandrade98@gmail.com>
2017-11-01 09:19:12 -0300

    unfix 001

--- vitor@load-books-by-author.js
+++ vitor@load-books-by-author.js
@@ -1,22 +1,46 @@
 var data = {
-    "_id" : 1.0,
+    "_id" : 150947978.0,
     "nome_livro" : "Livro 2",
-    "nome_autor" : "Autor 1",
+    "nome_autor" : [
+        {autor_1: "Vitor Cruz"}
+    ],
     "tipo" : "hardcover",
     "isbn" : "192-182-381-38",
     "editora" : "Editora 1",
     "n_paginas" : 100.0
 }
 
+/*
+-------------------transformar esse db.shell() em runCommand()----------
+db.books.aggregate(
+    [ { $match : { "nome_autor.autor_1" : "Vitor Cruz" } } ]
+);
+*/
+
 db.runCommand({
     aggregate: "books",
     
     pipeline: [
-        {
-            $match: {"nome_autor" : {
-                $regex : ".*"+data.nome_autor+".*", $options : "i"
+        
+            match: {
+                "nome_autor.autor_1" : {
+                    $regex : ".*"+data.nome_autor.autor_1+".*", $options : "i"
                 }
             }
-        }
+            //$unwind: nome_autor,
+            /* $elementMatch: {
+                $or: {
+                    "autor_1" : {
+                        $regex : ".*"+data.nome_autor.autor_1+".*", $options : "i"
+                    },
+                    "autor_2" : {
+                        $regex : ".*"+data.nome_autor.autor_1+".*", $options : "i"
+                    },
+                    "autor_3" : {
+                        $regex : ".*"+data.nome_autor.autor_1+".*", $options : "i"
+                    }
+                }
+            } */
+        
     ]
 } );
